2c5151f613f40fdcdc703095583a1609e28fa661
Daniel Velez <velezdaniel516@gmail.com>
2025-06-14 15:41:09 -0400

    add index.php template file

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+    include 'includes/header.php';
+?>
+
+<!--Home Section-->
+<section id="home">
+    <div class="container">
+
+    </div>
+</section>
+
+<!--About Section-->
+<section id="about">
+    <div class="container">
+
+    </div>
+</section>
+
+<!--Services Section-->
+<section id="services">
+    <div class="container">
+
+    </div>
+</section>
+
+<!--Projects Section-->
+<section id="projects">
+    <div class="container">
+
+    </div>
+</section>
+
+<!--Contact Us Section-->
+<section id="contact">
+    <div class="container">
+
+    </div>
+</section>
+
+<?php
+    include 'includes/footer.php';
+?>
