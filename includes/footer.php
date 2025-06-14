dae73772dcddcdbd606e12a2c6dcefff642c5ec1
Daniel Velez <velezdaniel516@gmail.com>
2025-06-14 15:41:30 -0400

    add header nav bar and footer template

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+</main>
+    <footer>
+        <div class="container">
+            <p>Copyright &copy; <?php echo date("Y"); ?> Daniel Velez. All rights reserved.</p>    
+        </div>
+        
+    </footer>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Web Portfolio</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" type="image/x-icon" href="favicon/favicon.ico">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous"><!-- Bootstrap CSS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script><!-- Bootstrap JS -->
+    <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet"><!-- Boxicons CSS -->
+</head>
+<body>
+    <header>
+        <div class="container">
+            <nav class="navbar navbar-expand-lg">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="index.php">Daniel Velez</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="index.php#home">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php#about">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php#services">Services</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php#projects">Projects</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php#contact">Contact Us</a>
+                        </li>
+                    </ul>
+
+                    <!--Creator Git Hub Link-->
+                    <ul class="navbar-nav ms-lg-auto mb-2 mb-lg-0 iconContainer">
+                        <li class="nav-item">
+                            <a href="https://github.com/Daniel1000c" target="_blank" class="btn btn-dark d-inline-flex align-items-center w-auto">
+                                <i class='bx bxl-github me-1'></i> GitHub
+                            </a>
+                        </li>
+                    </ul>
+
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </header>
+    <main>
