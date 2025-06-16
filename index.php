91db6bc4f0b162dcba1f4469338642fac5487e0e
Daniel Velez <velezdaniel516@gmail.com>
2025-06-16 15:08:01 -0400

    add hero section

--- index.php
+++ index.php
@@ -3,9 +3,15 @@
 ?>
 
 <!--Home Section-->
-<section id="home">
+<section id="home" class="heroSection">
     <div class="container">
-
+        <h1>Hi, I'm <span>Daniel</span></h1>
+        <p>Welcome to my Portfolio, where I showcase my skills and experience.</p>
+        <!--Button Container-->
+        <div class="btnContainer">
+            <a href="index.php#contact">Contact</a>
+           <a href="images/Daniel_Velez_Resume.pdf" download="Daniel_Velez_Resume.pdf">Resume</a>
+        </div>
     </div>
 </section>
 
