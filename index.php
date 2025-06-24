7cfd77ad0cca3f2f58a0b0c274f19363f1bbbfea
Daniel Velez <velezdaniel516@gmail.com>
2025-06-23 20:16:04 -0400

    add about section

--- index.php
+++ index.php
@@ -16,30 +16,62 @@
 </section>
 
 <!--About Section-->
-<section id="about">
+<section id="about" class="alternatingTheme aboutSection">
     <div class="container">
+        <h6>About Me</h6>
+        <h1>Get to Know <span>Me</span></h1>
+        <p>
+            I am a web development student from Broward College looking to expand my skills.
+            I want to broaden my horizons and help to create beautiful projects.
+        </p>
+        <a href="index.php#contact">Learn More &rarr;</a>
 
+        <!--Row Container-->
+        <div class="row align-items-center mt-5 creatorContainer">
+
+           <!--Column Items-->
+
+           <!--Creator Image-->
+           <div class="col-12 col-md-6 mb-4 mb-md-0 creatorImg">
+                <img src="images/creatorPicture.jpg" alt="Daniel Velez: Web Developer">
+           </div>
+
+           <!--Creator Info-->
+           <div class="col-12 col-md-6 mb-4 mb-md-0 creatorInfo">
+                <h3><span>My</span> Mission</h3>
+                <p>
+                    I am a web development student from Broward College looking to expand my skills.
+                    I want to broaden my horizons and help to create beautiful projects.
+                </p>
+                <ul>
+                    <li> Time Management</li>
+                    <li> Real-time Collaborator</li>
+                    <li> Effective Communication</li>
+                </ul>
+           </div>
+        </div>
+    
     </div>
 </section>
 
 <!--Services Section-->
-<section id="services">
+<section id="services" class="alternatingTheme">
     <div class="container">
-
+        <h2>template text</h2>
     </div>
 </section>
 
 <!--Projects Section-->
-<section id="projects">
+<section id="projects" class="alternatingTheme">
     <div class="container">
-
+        <h2>template text</h2>
     </div>
 </section>
 
 <!--Contact Us Section-->
-<section id="contact">
+<section id="contact" class="alternatingTheme">
     <div class="container">
-
+        <h2>template text</h2>
     </div>
 </section>
 
