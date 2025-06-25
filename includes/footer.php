76c629c3eea735c45aacd9cb209acf553a3a132d
Daniel Velez <velezdaniel516@gmail.com>
2025-06-25 14:45:52 -0400

    add footer icons and hr rule

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,16 @@
 </main>
     <footer>
         <div class="container">
-            <p>Copyright &copy; <?php echo date("Y"); ?> Daniel Velez. All rights reserved.</p>    
+            
+            <!--Create Footer Links-->
+            <a href="https://www.linkedin.com/in/daniel-velez-b17748286/" target="_blank" class="socialIcon"><i class="bx bxl-linkedin-square"></i></a>
+            <a href="mailto:velezdaniel6162@gmail.com" class="socialIcon"><i class="bx bxl-gmail"></i></a>
+            <a href="https://codepen.io/nliuknwq-the-builder" target="_blank" class="socialIcon"><i class="bx bxl-codepen"></i></a>
+
+            <hr>
+            <a href="#" class="footerLinks">Terms of Service</a>
+            <a href="#" class="footerLinks">Privacy Policy</a>
+            <p>Copyright &copy; <?php echo date("Y"); ?> Daniel Velez</p>    
         </div>
         
     </footer>
