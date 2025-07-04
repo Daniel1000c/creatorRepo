1b45c3fd21c4aa321bfb4ccaad32887f23e25851
Daniel Velez <velezdaniel516@gmail.com>
2025-07-04 18:19:13 -0400

    add stick header function and appear on scroll content function

--- scripts/script.js
+++ scripts/script.js
@@ -25,3 +25,38 @@ const observer = new IntersectionObserver (
 sections.forEach((section) => {
     observer.observe(section);
 });
+
+//Create function to change header theme on scroll
+
+//Get header element
+const header = document.querySelector("header");
+
+//Create scroll toggle function
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");//Remove scrolled class
+    } 
+});
+
+//Get all sections
+const scrollSections = document.querySelectorAll('section .container');
+
+// Create an intersection observer
+const scrollObserver = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        // If the section is in view, add the "visible" class
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+            observer.unobserve(entry.target);  // Stop observing once it enters the viewport
+        }
+    });
+}, {
+    threshold: 0.5 // Adjust this value to control when the animation triggers (0.5 = 50% of the section is visible)
+});
+
+// Observe each section
+scrollSections.forEach(container => {
+    scrollObserver.observe(container);
+});
