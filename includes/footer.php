618bc734fad0dbff6972baefe84c132d77eae5a7
Daniel Velez <velezdaniel516@gmail.com>
2025-07-19 16:25:34 -0400

    add aria labels to empty footer linnks

--- includes/footer.php
+++ includes/footer.php
@@ -3,9 +3,9 @@
         <div class="container">
             
             <!--Create Footer Links-->
-            <a href="https://www.linkedin.com/in/daniel-velez-b17748286/" target="_blank" class="socialIcon"><i class="bx bxl-linkedin-square"></i></a>
-            <a href="mailto:velezdaniel6162@gmail.com" class="socialIcon"><i class="bx bxl-gmail"></i></a>
-            <a href="https://codepen.io/nliuknwq-the-builder" target="_blank" class="socialIcon"><i class="bx bxl-codepen"></i></a>
+            <a href="https://www.linkedin.com/in/daniel-velez-b17748286/" target="_blank" class="socialIcon" aria-label="LinkedIn Profile Daniel Velez"><i class="bx bxl-linkedin-square"></i></a>
+            <a href="mailto:velezdaniel6162@gmail.com" class="socialIcon" aria-label="Gmail Daniel Velez"><i class="bx bxl-gmail"></i></a>
+            <a href="https://codepen.io/nliuknwq-the-builder" target="_blank" class="socialIcon" aria-label="CodePen Daniel Velez"><i class="bx bxl-codepen"></i></a>
 
             <hr>
             <p>Copyright &copy; <?php echo date("Y"); ?> Daniel Velez</p>    
