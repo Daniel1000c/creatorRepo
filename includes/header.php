7fbe26aa26d283f990c11cf528ed86289ef0096a
Daniel Velez <velezdaniel516@gmail.com>
2025-08-14 17:21:03 -0400

    remove previous navigation bar and create navbar template

--- includes/header.php
+++ includes/header.php
@@ -1,55 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Web Portfolio</title>
-    <link rel="icon" type="image/x-icon" href="favicon/favicon.ico">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous"><!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="css/style.css">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script><!-- Bootstrap JS -->
-    <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet"><!-- Boxicons CSS -->
-</head>
-<body>
-    <header>
-        <div class="container">
-            <nav class="navbar navbar-expand-lg">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="index.php"><span>Daniel</span> Velez</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="index.php#home">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="index.php#about">About</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="index.php#services">Services</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="index.php#projects">Projects</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="index.php#contact">Contact Us</a>
-                        </li>
-                    </ul>
-
-                    <!--Creator Git Hub Link-->
-                    <ul class="navbar-nav ms-lg-auto mb-2 mb-lg-0 iconContainer">
-                        <li class="nav-item">
-                            <a href="https://github.com/Daniel1000c" target="_blank" class="btn btn-dark d-inline-flex align-items-center w-auto">
-                                <i class='bx bxl-github me-1'></i> GitHub
-                            </a>
-                        </li>
-                    </ul>
-
-                    </div>
-                </div>
-            </nav>
-        </div>
-    </header>
-    <main>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png">
+        <link rel="manifest" href="./site.webmanifest">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    </head>
+    <body>
+        <main>
+            <p>Hello</p>
