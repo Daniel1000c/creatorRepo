2ca0203246f8c1539f6aacb1e6a9cbcb1ff17a98
Daniel Velez <velezdaniel516@gmail.com>
2025-08-22 17:48:46 -0400

    import navigation bar from components

--- work-portfolio/src/App.jsx
+++ work-portfolio/src/App.jsx
@@ -1,8 +1,11 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navigation from "./components/navbar";
 
 function App() {
   return (
     <>
+      <Navigation />
       <p>Hello World</p>
       <p>Test</p>
     </>
