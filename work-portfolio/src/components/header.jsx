2c61906f524d838b8f5b5f5ad2998db7c80a2a3b
Daniel Velez <velezdaniel516@gmail.com>
2025-08-23 19:04:55 -0400

    Import navigation bar component and create header container

---
+++ work-portfolio/src/components/header.jsx
@@ -0,0 +1,11 @@
+//Import navigation bar
+import Navigation from "./navbar";
+function Header() {
+  return (
+    <header className="p-1">
+      <Navigation />
+    </header>
+  );
+}
+
+export default Header;
