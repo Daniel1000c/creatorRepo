d405f55c5d18a7e91b1bdab7309ee9bb484be26a
Daniel Velez <velezdaniel516@gmail.com>
2025-08-28 16:01:12 -0400

    Remove template components

--- work-portfolio/src/App.jsx
+++ work-portfolio/src/App.jsx
@@ -1,23 +1,8 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Header from "./components/header"; //Header component
-import Footer from "./components/footer"; //Footer component
-import Hero from "./components/hero"; //Hero component
-import About from "./components/about"; //About component
-import Contact from "./components/contact"; //Contact component
-import Portfolio from "./components/portfolio"; //Portfolio component
 
 function App() {
-  return (
-    <>
-      <Header />
-      <Hero />
-      <About />
-      <Portfolio />
-      <Contact />
-      <Footer />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
