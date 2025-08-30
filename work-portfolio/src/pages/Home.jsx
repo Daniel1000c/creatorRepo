6200b4ba103408fab929ec60e4773d3a9114d082
Daniel Velez <velezdaniel516@gmail.com>
2025-08-30 19:48:51 -0400

    Add pages folder
    -Create pages folder to hold route pages
    -Create home page route
    -Create page not found route

---
+++ work-portfolio/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+const Home = () => {
+  return (
+    <div className="min-h-screen">
+      {/*Dark Mode toggle */}
+
+      {/*Background */}
+
+      {/*Navigation bar */}
+
+      {/*Main Content */}
+
+      {/*Footer */}
+    </div>
+  );
+};
+
+export default Home;
---
+++ work-portfolio/src/pages/PageNotFound.jsx
@@ -0,0 +1,9 @@
+const PageNotFound = () => {
+  return (
+    <section>
+      <h1>Page Not Found</h1>
+    </section>
+  );
+};
+
+export default PageNotFound;
