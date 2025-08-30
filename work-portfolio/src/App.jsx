e3a0221e9e89de314609cfdfecd125a70058c417
Daniel Velez <velezdaniel516@gmail.com>
2025-08-30 19:55:51 -0400

    Add browserrouter and routes for 404 and home page

--- work-portfolio/src/App.jsx
+++ work-portfolio/src/App.jsx
@@ -1,11 +1,24 @@
-import Header from "./components/Header"; // Header component
-import Footer from "./components/Footer"; // Footer component
-
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
+import Home from "./pages/Home"; // Home page
+import PageNotFound from "./pages/pageNotFound"; // 404 Page
 function App() {
   return (
     <>
-      <Header />
-      <Footer />
+      <BrowserRouter>
+        <Routes>
+          {/*Home Page Route */}
+          <Route index element={<Home />} />
+
+          {/*404 Page Route */}
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
